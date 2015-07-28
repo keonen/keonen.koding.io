d0e36429032ee0b29606cf9f190a06d9a9f9560e
keonen <keijo.olavi.kangas@gmail.com>
2015-07-28 20:10:29 +0000

    2015-07-28 23:10

---
+++ app.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
+app.get('/test', function (req, res) {
+  res.send('This is a test!');
+});
+
+var server = app.listen(3000, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Example app listening at http://%s:%s', host, port);
+});
