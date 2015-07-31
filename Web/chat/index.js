e04638e01c86bf35e39afc5a26fc1da183d1aa9c
keonen <keijo.olavi.kangas@gmail.com>
2015-07-31 06:45:37 +0000

    2015-07-31 09:45

---
+++ Web/chat/index.js
@@ -0,0 +1,23 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.get('/', function(req, res){
+   
+    res.sendfile('index.html');
+});
+
+// Wait connection
+io.on('connection', function(socket){
+   
+    console.log('user connected');
+    socket.emit("connection created", {viesti:'olet yhteydessä'});
+    
+    socket.on('chatbot', function(data){
+        io.emit('check chatmessage', data);
+    });
+});
+
+http.listen(3000, function(){
+    console.log('listening on *:3000');
+});
